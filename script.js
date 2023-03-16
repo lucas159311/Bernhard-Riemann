7386a1fd889c8eb487e2c27ce8d437195509aff6
Lucas Avelino <50250488+lucas159311@users.noreply.github.com>
2023-03-15 22:15:40 -0300

    adicionando calculo da integral
    adicionando função que calcula a integral de 0 a um numero escolhido pelo usuário.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+function CalculaIntegral(){
+    var sup = document.getElementById('limite').value;
+    var result = ((sup*sup*sup)/3);
+    console.log(result);
+    return result;
+}
+
 function Calcular(){
     if(verificaValor()){
         var area = 0;
         var y = 0;
-        var integral = 21.333333;
+        var integral = Number.parseFloat(CalculaIntegral());
+        var sup = document.getElementById('limite').value;
 
         var intervalo = Number.parseFloat(document.getElementById('intervalo').value);
-        for(var i=0; i < 4; i+=intervalo){
+        for(var i=0; i < sup; i+=intervalo){
             console.log("passei "+i+" vezes");
             y = i*i;
             area += (intervalo*y);
@@ -13,20 +21,21 @@ function Calcular(){
         //exibe o resultado
         document.getElementById('resultado').innerHTML = "A área dos retangulos: <b>" + area.toFixed(2) + "</b>";
         document.getElementById('resultado').innerHTML += "<br>A área calculada pela integral: <b>" + integral.toFixed(2) + "</b>";
-        document.getElementById('resultado').innerHTML += "<br>A taxa de erro: <b>" + setError(area).toFixed(2) + "%</b>";
+        document.getElementById('resultado').innerHTML += "<br>A taxa de erro: <b>" + setError(area,integral).toFixed(2) + "%</b>";
     }else{
-        window.alert("Digite um valor no campo para continuar!");
+        window.alert("Complete todos os campos para continuar");
     }
 
 }
 
 function Limpar(){
     document.getElementById('resultado').innerHTML = "";
+    document.getElementById('limite').value = "";
     document.getElementById('intervalo').value = "";
 }
 
-function setError(area){
-    var result = (100-((area/21.333333)*100));
+function setError(area,integral){
+    var result = (100-((area/integral)*100));
     if(result < 0){//negativo
         return result * -1;
     }else{
@@ -35,7 +44,7 @@ function setError(area){
 }
 
 function verificaValor(){
-    if(document.getElementById('intervalo').value == ""){
+    if(document.getElementById('intervalo').value == "" || document.getElementById('limite').value == ""){
         return 0;
     }else{
         return 1;
